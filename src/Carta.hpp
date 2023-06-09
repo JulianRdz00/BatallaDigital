870de16110e2dd5a47641a796ed2707af2269e67
Franco-Favotti <131703906+Franco-Favotti@users.noreply.github.com>
2023-06-09 16:44:27 -0300

    Carta.hpp

--- src/Carta.hpp
+++ src/Carta.hpp
@@ -6,6 +6,25 @@ private:
     enum nombre{ASALTO, BOMBA, RADAR, ETC};
     unsigned int id;
 public:
+    /*Pre: El id debe ser mayor a 0
+	*Post: Crea la carta con el nombre e id indicado
+	*/
+	Carta crearCarta(nombreCarta nombre, unsigned int id);
+
+	/*Pre: -
+	*Post: Elimina la carta
+	*/
+	virtual~Carta();
+
+	/*Pre:-
+	*Post: Devuelve el nombre de la carta
+	*/
+	nombreCarta getNombre();
+
+	/*Pre:-
+	*Post: Devuelve el id de la carta
+	*/
+	unsigned int getId();
 
 };
 
