43affd92ea5d3e6f0f286009dc5d4d6d34907168
Juan Manuel Mendez <103156703+Chaile-Mendez@users.noreply.github.com>
2023-06-12 10:39:38 -0300

    Se edita el main, se agregan constantes por defecto y se comienza a estructurar los constructores de Juego

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,39 @@
 
 #include "Constantes.hpp"
 #include "Juego.hpp"
+// PRE: La respuesta introducida por consola debe ser un numero.
+// POST: Pregunta por consola que se introduzca un numero entero positivo, continuara preguntando hasta que la respuesta sea valida
+int preguntarEnteroPositivo(std::string pregunta){
+    int variable=0;
+    while(variable < 1){
+        std::cout << pregunta << std::endl;
+        std::cout << "Debe ser un numero positivo" << std::endl;
+        std::cin << variable;
+    }
+    return variable;
+}
+
+
+//Post devuelve un puntero a un Juego con unos parametros elejidos por el usuario.
+Juego* inicializarPartida(){
+    int ancho = preguntarEnteroPositivo("ingrese el Ancho del tablero.");
+    int largo = preguntarEnteroPositivo("ingrese el Ancho del tablero.");
+    int alto = preguntarEnteroPositivo("ingrese el Ancho del tablero.");
+
+    int cantidadJugadores = preguntarEnteroPositivo("ingrese la cantidad de jugadores.");
+    int cantidadSoldadosPorJugador = preguntarEnteroPositivo("ingrese la cantidad de soldados iniciales para cada jugador.");
+
+    Juego* partida = new Juego(ancho, largo, alto, cantidadJugadores, cantidadSoldadosPorJugador);
+}
+
 
 int main()
 {
 
     srand((unsigned)time(NULL));
-    Coordenada puntoMasLejano = Coordenada(10, 10, 10);
+
+
+
 
     Juego *batallaDigital = new Juego();
 
