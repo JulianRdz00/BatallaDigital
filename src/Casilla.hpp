2ed5d5cb873bbcf80736a7a034976376c77edec9
julian <julian.rodriguez086@gmail.com>
2023-06-14 22:34:26 -0300

    fix conflicts

--- src/Casilla.hpp
+++ src/Casilla.hpp
@@ -1,28 +1,17 @@
 #ifndef _CASILLA_H_
 #define _CASILLA_H_
 
-<<<<<<< HEAD
 #include "Coordenada.hpp"
-=======
 #include "Unidad.hpp"
->>>>>>> 88958a0ef9b45bc82e636766c0068c5fb73f3eae
 #include "Constantes.hpp"
 
 
 class Casilla
 {
 private:
-<<<<<<< HEAD
-    Coordenada ubicacion;
-    Jugador *jugadorPosicion;
-    tipoOcupante ocupante;
-    int turnosInactiva;
-    tipoTerreno terreno;
-=======
     Unidad *unidad;
     TipoTerreno terreno;
 
->>>>>>> 88958a0ef9b45bc82e636766c0068c5fb73f3eae
 public:
     Casilla()
     {
